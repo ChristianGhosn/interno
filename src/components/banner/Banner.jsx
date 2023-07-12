4eb14bc363078c24966d44431019fda038b0544a
ChristianGhosn <christianjghosn@gmail.com>
2023-07-12 20:03:37 +1000

    changed background image url from src/assets/ to assets/

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,10 +3,7 @@ import "./banner.css";
 
 const Banner = ({ image, heading, parent, child }) => {
   return (
-    <div
-      className="banner"
-      style={{ backgroundImage: `url(../assets/${image})` }}
-    >
+    <div className="banner" style={{ backgroundImage: `url(assets/${image})` }}>
       <div className="banner_div">
         <h1 className="heading-1">{heading}</h1>
         <p className="text">
